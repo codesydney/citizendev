f996105927f3a8fc6778291deef12e20f9144469
Engramar Bollas <engramar.bollas@gmail.com>
2024-02-25 07:42:02 +1100

    PY4E

--- app.py
+++ app.py
@@ -29,10 +29,24 @@ def signup():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        task1 = 'To Do'
-        task2 = 'To Do'
+        PY4E1 = 'To Do'
+        PY4E2 = 'To Do'
+        PY4E3 = 'To Do'
+        PY4E4 = 'To Do'     
+        PY4E5 = 'To Do'
+        PY4E6 = 'To Do'
+        PY4E7 = 'To Do'
+        PY4E8 = 'To Do'
+        PY4E9 = 'To Do'
+        PY4E10 = 'To Do'
+        PY4E11 = 'To Do'
+        PY4E12 = 'To Do'     
+        PY4E13 = 'To Do'
+        PY4E14 = 'To Do'
+        PY4E15 = 'To Do'
+        PY4E16 = 'To Do'                    
         try:
-            c.execute("INSERT INTO user (username, password, task1,  task2) VALUES (?, ?, ?, ?)", (username, password, task1, task2))
+            c.execute("INSERT INTO user (username, password, PY4E1, PY4E2, PY4E3, PY4E4, PY4E5, PY4E6, PY4E7, PY4E8, PY4E9, PY4E10, PY4E11, PY4E12, PY4E13, PY4E14, PY4E15, PY4E16 ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", (username, password, PY4E1, PY4E2, PY4E3, PY4E4, PY4E5, PY4E6, PY4E7, PY4E8, PY4E9, PY4E10, PY4E11, PY4E12, PY4E13, PY4E14, PY4E15, PY4E16))
             conn.commit()
             session['username'] = username
             return redirect(url_for('dashboard'))
@@ -55,7 +69,21 @@ def update(username):
     if request.method == 'POST':
         status1 = request.form['status1']
         status2 = request.form['status2']
-        c.execute("UPDATE user SET task1=?, task2=? WHERE username=?", (status1, status2, username))
+        status3 = request.form['status3']
+        status4 = request.form['status4']
+        status5 = request.form['status5']
+        status6 = request.form['status6']
+        status7 = request.form['status7']
+        status8 = request.form['status8']
+        status9 = request.form['status9']
+        status10 = request.form['status10']
+        status11 = request.form['status11']
+        status12 = request.form['status12']
+        status13 = request.form['status13']
+        status14 = request.form['status14']
+        status15 = request.form['status15']
+        status16 = request.form['status16']
+        c.execute("UPDATE user SET PY4E1=?, PY4E2=?, PY4E3=?, PY4E4=?, PY4E5=?, PY4E6=?, PY4E7=?, PY4E8=?, PY4E9=?, PY4E10=?, PY4E11=?, PY4E12=?, PY4E13=?, PY4E14=?, PY4E15=?, PY4E16=? WHERE username=?", (status1, status2, status3, status4, status5, status6, status7, status8, status9, status10, status11, status12, status13, status14, status15, status16, username))
         conn.commit()
         return redirect(url_for('dashboard'))
     else:
