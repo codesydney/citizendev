6f8a6d7a4209a4f7247144ec042575cc310264e5
Engramar Bollas <engramar.bollas@gmail.com>
2024-02-24 17:53:14 +1100

    Tiny end-to-end working version

--- app.py
+++ app.py
@@ -31,20 +31,37 @@ def signup():
         password = request.form['password']
         task1 = 'To Do'
         task2 = 'To Do'
-        c.execute("INSERT INTO user (username, password, task1,  task2) VALUES (?, ?, ?, ?)", (username, password, task1, task2))
-        conn.commit()
-        session['username'] = username
-        return redirect(url_for('dashboard'))
-    return render_template('signup.html')
+        try:
+            c.execute("INSERT INTO user (username, password, task1,  task2) VALUES (?, ?, ?, ?)", (username, password, task1, task2))
+            conn.commit()
+            session['username'] = username
+            return redirect(url_for('dashboard'))
+        except sqlite3.IntegrityError:
+            # If the username already exists, show an error message
+            return render_template('signup.html', error=True)
+    return render_template('signup.html', error=False)
 
 @app.route('/dashboard')
 def dashboard():
     if 'username' in session:
         username = session['username']
+        c.execute("SELECT * FROM user")
+        users = c.fetchall()
+        return render_template('dashboard.html', username=username, users=users)
+    return redirect(url_for('login'))
+
+@app.route('/update/<username>', methods=['GET', 'POST'])
+def update(username):
+    if request.method == 'POST':
+        status1 = request.form['status1']
+        status2 = request.form['status2']
+        c.execute("UPDATE user SET task1=?, task2=? WHERE username=?", (status1, status2, username))
+        conn.commit()
+        return redirect(url_for('dashboard'))
+    else:
         c.execute("SELECT * FROM user WHERE username=?", (username,))
         user = c.fetchone()
-        return render_template('dashboard.html', username=username, user=user)
-    return redirect(url_for('login'))
+        return render_template('update.html', username=username, user=user)
 
 @app.route('/logout')
 def logout():
