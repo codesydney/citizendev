708275a4e5b5fa3b2ab71b305963a1083aab6fea
Engramar Bollas <engramar.bollas@gmail.com>
2024-02-26 20:20:31 +1100

    Added DJ4E

--- app.py
+++ app.py
@@ -29,24 +29,11 @@ def signup():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        PY4E1 = 'To Do'
-        PY4E2 = 'To Do'
-        PY4E3 = 'To Do'
-        PY4E4 = 'To Do'     
-        PY4E5 = 'To Do'
-        PY4E6 = 'To Do'
-        PY4E7 = 'To Do'
-        PY4E8 = 'To Do'
-        PY4E9 = 'To Do'
-        PY4E10 = 'To Do'
-        PY4E11 = 'To Do'
-        PY4E12 = 'To Do'     
-        PY4E13 = 'To Do'
-        PY4E14 = 'To Do'
-        PY4E15 = 'To Do'
-        PY4E16 = 'To Do'                    
+        PY4E1 = PY4E2 = PY4E3 = PY4E4 = PY4E5 = PY4E6 = PY4E7 = PY4E8 = PY4E9 = PY4E10 = PY4E11 = PY4E12 = PY4E13 = PY4E14 = PY4E15 = PY4E16 = PY4E17 = 'To Do'                    
+        DJ4E1 = DJ4E2 = DJ4E3 = DJ4E4 = DJ4E5 = DJ4E6 = DJ4E7 = DJ4E8 = DJ4E9 = DJ4E10 = DJ4E11 = DJ4E12 = DJ4E13 = DJ4E14 = DJ4E15 = DJ4E16 = DJ4E17 = DJ4E18 = DJ4E19 = DJ4E20 = DJ4E21 = DJ4E22 = DJ4E23 = DJ4E24 = DJ4E25 = DJ4E26 = DJ4E27 = DJ4E28  = DJ4E29 = DJ4E30 = DJ4E31 = DJ4E32 = DJ4E33 = DJ4E34 = DJ4E35 = DJ4E36 = DJ4E37 = DJ4E38 = DJ4E39 = DJ4E40 = 'To Do'
+
         try:
-            c.execute("INSERT INTO user (username, password, PY4E1, PY4E2, PY4E3, PY4E4, PY4E5, PY4E6, PY4E7, PY4E8, PY4E9, PY4E10, PY4E11, PY4E12, PY4E13, PY4E14, PY4E15, PY4E16 ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", (username, password, PY4E1, PY4E2, PY4E3, PY4E4, PY4E5, PY4E6, PY4E7, PY4E8, PY4E9, PY4E10, PY4E11, PY4E12, PY4E13, PY4E14, PY4E15, PY4E16))
+            c.execute("INSERT INTO user (username, password, PY4E1, PY4E2, PY4E3, PY4E4, PY4E5, PY4E6, PY4E7, PY4E8, PY4E9, PY4E10, PY4E11, PY4E12, PY4E13, PY4E14, PY4E15, PY4E16, PY4E17, DJ4E1, DJ4E2, DJ4E3, DJ4E4, DJ4E5, DJ4E6, DJ4E7, DJ4E8, DJ4E9, DJ4E10, DJ4E11, DJ4E12, DJ4E13, DJ4E14, DJ4E15, DJ4E16, DJ4E17, DJ4E18, DJ4E19, DJ4E20, DJ4E21, DJ4E22, DJ4E23, DJ4E24, DJ4E25, DJ4E26, DJ4E27, DJ4E28 , DJ4E29, DJ4E30, DJ4E31, DJ4E32, DJ4E33, DJ4E34, DJ4E35, DJ4E36, DJ4E37, DJ4E38, DJ4E39, DJ4E40) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", (username, password, PY4E1, PY4E2, PY4E3, PY4E4, PY4E5, PY4E6, PY4E7, PY4E8, PY4E9, PY4E10, PY4E11, PY4E12, PY4E13, PY4E14, PY4E15, PY4E16, PY4E17, DJ4E1, DJ4E2, DJ4E3, DJ4E4, DJ4E5, DJ4E6, DJ4E7, DJ4E8, DJ4E9, DJ4E10, DJ4E11, DJ4E12, DJ4E13, DJ4E14, DJ4E15, DJ4E16, DJ4E17, DJ4E18, DJ4E19, DJ4E20, DJ4E21, DJ4E22, DJ4E23, DJ4E24, DJ4E25, DJ4E26, DJ4E27, DJ4E28 , DJ4E29, DJ4E30, DJ4E31, DJ4E32, DJ4E33, DJ4E34, DJ4E35, DJ4E36, DJ4E37, DJ4E38, DJ4E39, DJ4E40))
             conn.commit()
             session['username'] = username
             return redirect(url_for('dashboard'))
@@ -67,6 +54,7 @@ def dashboard():
 @app.route('/app/update/<username>', methods=['GET', 'POST'])
 def update(username):
     if request.method == 'POST':
+        #PY4E
         status1 = request.form['status1']
         status2 = request.form['status2']
         status3 = request.form['status3']
@@ -83,7 +71,50 @@ def update(username):
         status14 = request.form['status14']
         status15 = request.form['status15']
         status16 = request.form['status16']
-        c.execute("UPDATE user SET PY4E1=?, PY4E2=?, PY4E3=?, PY4E4=?, PY4E5=?, PY4E6=?, PY4E7=?, PY4E8=?, PY4E9=?, PY4E10=?, PY4E11=?, PY4E12=?, PY4E13=?, PY4E14=?, PY4E15=?, PY4E16=? WHERE username=?", (status1, status2, status3, status4, status5, status6, status7, status8, status9, status10, status11, status12, status13, status14, status15, status16, username))
+        status17 = request.form['status17']
+        #DJ4E
+        status18 = request.form['status18']
+        status19 = request.form['status19']
+        status20 = request.form['status20']
+        status21 = request.form['status21']        
+        status22 = request.form['status22']        
+        status23 = request.form['status23']        
+        status24 = request.form['status24']        
+        status25 = request.form['status25']        
+        status26 = request.form['status26']        
+        status27 = request.form['status27']        
+        status28 = request.form['status28']        
+        status29 = request.form['status29']        
+        status30 = request.form['status30']        
+        status31 = request.form['status31']        
+        status32 = request.form['status32']      
+        status33 = request.form['status33']      
+        status34 = request.form['status34']      
+        status35 = request.form['status35']      
+        status36 = request.form['status36']      
+        status37 = request.form['status37']      
+        status38 = request.form['status38']                                                      
+        status39 = request.form['status39']      
+        status40 = request.form['status40']   
+        status41 = request.form['status41']   
+        status42 = request.form['status42']   
+        status43 = request.form['status43']   
+        status44 = request.form['status44']   
+        status45 = request.form['status45']   
+        status46 = request.form['status46']   
+        status47 = request.form['status47']                                                      
+        status48 = request.form['status48']   
+        status49 = request.form['status49']   
+        status50 = request.form['status50']   
+        status51 = request.form['status51']   
+        status52 = request.form['status52']   
+        status53 = request.form['status53']   
+        status54 = request.form['status54']   
+        status55 = request.form['status55']   
+        status56 = request.form['status56']   
+        status57 = request.form['status57']   
+        c.execute("UPDATE user SET PY4E1=?, PY4E2=?, PY4E3=?, PY4E4=?, PY4E5=?, PY4E6=?, PY4E7=?, PY4E8=?, PY4E9=?, PY4E10=?, PY4E11=?, PY4E12=?, PY4E13=?, PY4E14=?, PY4E15=?, PY4E16=?, PY4E17=?, DJ4E18=?, DJ4E2=?, DJ4E3=?, DJ4E4=?, DJ4E5=?, DJ4E6=?, DJ4E7=?, DJ4E8=?, DJ4E9=?, DJ4E10=?, DJ4E11=?, DJ4E12=?, DJ4E13=?, DJ4E14=?, DJ4E15=?, DJ4E16=?, DJ4E17=?, DJ4E18=?, DJ4E19=?, DJ4E20=?, DJ4E21=?, DJ4E22=?, DJ4E23=?, DJ4E24=?, DJ4E25=?, DJ4E26=?, DJ4E27=?, DJ4E28 =?, DJ4E29=?, DJ4E30=?, DJ4E31=?, DJ4E32=?, DJ4E33=?, DJ4E34=?, DJ4E35=?, DJ4E36=?, DJ4E37=?, DJ4E38=?, DJ4E39=?, DJ4E40=? WHERE username=?", (status1, status2, status3, status4, status5, status6, status7, status8, status9, status10, status11, status12, status13, status14, status15, status16, status17, status18, status19, status20, status21, status22, status23, status24, status25, status26, status27, status28, status29, status30, status31, status32, status33, status34, status35, status36, status37, status38, status39, status40, status41, status42, status43, status44, status45, status46, status47, status48, status49, status50, status51, status52, status53, status54, status55, status56, status57, username))
+ 
         conn.commit()
         return redirect(url_for('dashboard'))
     else:
